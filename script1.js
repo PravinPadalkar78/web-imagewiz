03e2018c751476c6bb85a707a2faafb42980b477
pravin padalkar <pravinpadalkar78@gmail.com>
2024-08-24 21:35:32 +0530

    worked on functionality

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
 
 //function for file loading
-window.onload = function () {
+window.onload = async function () {
     const image_input = document.querySelector("#image_input");
-    var uploaded_img = "";
-
+    let uploaded_img = "";
     image_input.addEventListener("change", () => {
         const reader = new FileReader();
         reader.addEventListener("load", () => {
@@ -49,7 +48,7 @@ function downloadBtn() {
     let img = document.querySelector(".display_image");
     let canvas = document.getElementById("myCanva");
     let ctx = canvas.getContext("2d");
-    
+
 
     canvas.width = img.width;
     canvas.height = img.height;
@@ -70,4 +69,39 @@ function downloadBtn() {
     // Trigger the download
     link.click();
     alert("Download succesfull!!!")
-}        
+}
+
+
+//function for cropping image
+async function cropBtn() {
+   
+    // let cropImg = document.querySelector('.Image_cropped');
+    // let cropper;
+
+    // if (cropper) {
+    //     cropper.destroy(); // Destroy the previous cropper instance
+    // }
+
+    // cropper =await new Cropper(cropImg, {
+    //     viewMode: 1,
+    //     responsive: true,
+    //     autoCropArea: 1, // Ensure the crop area covers the entire image
+    //     movable: true,
+    //     zoomable: true,
+    //     scalable: true,
+    //     cropBoxResizable: true,
+    //     cropBoxMovable: true,
+    //     // Set initial crop box data to match the entire image
+    //     data: {
+    //         x: 0,
+    //         y: 0,
+    //         width: cropImg.width,
+    //         height: cropImg.height
+    //     }
+    // });
+    // const canvas = cropper.getCroppedCanvas();
+    // console.log(cropper)
+    // cropImg.src = canvas.toDataURL();
+    alert("Currantly Working On it");
+}
+
---
+++ test.js
@@ -0,0 +1,5 @@
+function cropperactivate() {
+    alert("click")
+    let cropper = new cropper();
+
+}
